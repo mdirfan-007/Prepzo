f509d4d92c1e7ac86e0d84dec31848040d404fd8
MD IRFAN <mdimrfat007@gmail.com>
2026-04-04 16:19:05 +0530

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Button variant="gold">Subscribe</Button>
+      <Badge variant="gold">Gold Badge</Badge>
+    </div>
+  )
+}
+
+export default page
--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
         link: "text-primary underline-offset-4 hover:underline",
+        gold:"bg-amber-400 text-[#0a0a0b] hover:bg-amber-300 hover:-translate-y-0.5 hover:shadow-[0_12px_36px_rgba(251,191,36,0.35)]"
       },
       size: {
         default:
