763e02c8d52752b46f257eaca23d6602db630a2f
MD IRFAN <mdimrfat007@gmail.com>
2026-04-05 14:56:22 +0530

    clerk sign in and sign up separete rutes

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='mt-12'>
-      <Button variant="gold">Subscribe</Button>
+    <div>
+      
     </div>
   )
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,23 @@
 import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import React from "react";
 import { Button } from "./ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <nav className="fixed top-0 inset-x-0 z-50 flex items-center justify-between px-10 py-3 border-b border-white/7 backdrop-blur-xl">
       {/* Logo or Brand Name */}
-      Prepzo
+      <Link href={'/'}>
+       Prepzo
+      </Link>
       {/* Navigation Links */}
       <div className="flex items-center gap-3">
         <Show when="signed-out">
-          <SignInButton mode="modal">
+          <SignInButton >
             <Button variant="link">Sign In</Button>
           </SignInButton>
-          <SignUpButton mode="modal">
+          <SignUpButton >
             <Button variant="gold">Get started →</Button>
           </SignUpButton>
         </Show>
