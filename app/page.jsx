da9268ad7467c9c2554378f066fffb535b2d8916
MD IRFAN <mdimrfat007@gmail.com>
2026-04-14 15:54:36 +0530

    integrate supabase and arcjet and work on some little bit on landing page

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import { GoldTitle, GrayTitle, Sectionlabel } from '@/components/reusePara'
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import React from 'react'
@@ -5,7 +6,12 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      
+      <section className='pt-30 sm:pt-32 flex flex-col gap-14'>
+        <GoldTitle>Welcome to Our Website</GoldTitle>
+      <GrayTitle>Welcome to Prepzo</GrayTitle>
+      <Sectionlabel>Welcome to Prepzo</Sectionlabel>
+      </section>
+
     </div>
   )
 }
